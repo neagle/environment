93eedba658725b1cd06ebc39e00a7a7c1d64a163
Nate Eagle <n.eagle@gmail.com>
2013-01-04 15:18:23 -0500

    added jshintrc & vimrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,21 +8,55 @@ module.exports = function(grunt) {
       files: ['Gruntfile.js']
     },
     symlink: {
+
       'oh-my-zsh': {
         dest: '<%= home %>/.oh-my-zsh',
         relativeSrc: process.env.PWD + '/submodules/oh-my-zsh',
-	options: {
-	  type: 'dir'
-	}
+        options: {
+          type: 'dir'
+        }
       },
+
       zshrc: {
         dest: '<%= home %>/.zshrc',
         relativeSrc: process.env.PWD + '/dotfiles/zshrc'
       },
+
+      'eagle.zsh-theme': {
+        dest: 'submodules/oh-my-zsh/themes/eagle.zsh-theme',
+        relativeSrc: process.env.PWD + '/submodules/eagle.zsh-theme/eagle.zsh-theme'
+      },
+
       gitconfig: {
         dest: '<%= home %>/.gitconfig',
         relativeSrc: process.env.PWD + '/dotfiles/gitconfig'
+      },
+
+      vim: {
+        dest: '<%= home %>/.vim',
+        relativeSrc: process.env.PWD + '/submodules/vim',
+        options: {
+          type: 'dir'
+        }
+      },
+
+      // Vim Settings
+      vimrc: {
+        dest: '<%= home %>/.vimrc',
+        relativeSrc: process.env.PWD + '/submodules/vim/vimrc'
+      },
+
+      // A file for personal vim settings
+      'vimrc.settings': {
+        dest: '<%= home %>/.vimrc.settings',
+        relativeSrc: process.env.PWD + '/dotfiles/vimrc.settings'
+      },
+
+      'jshintrc': {
+        dest: '<%= home %>/.jshintrc',
+        relativeSrc: process.env.PWD + '/dotfiles/jshintrc'
       }
+
     },
     watch: {
       files: '<config:lint.files>',
@@ -47,7 +81,7 @@ module.exports = function(grunt) {
 
   // Load npm tasks
   grunt.loadNpmTasks('grunt-symlink');
- 
+
   grunt.registerTask('debug', 'A task to output debugging information.', function () {
     grunt.log.writeln('Debug this!');
 
@@ -55,6 +89,7 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', ['debug', 'symlink']);
+  grunt.registerTask('default', ['symlink']);
+  // grunt.registerTask('default', ['debug']);
 
 };
